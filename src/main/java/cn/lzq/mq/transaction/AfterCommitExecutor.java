469787feee284ec71078151463edad2d3a7c964a
lianzq <254663475@qq.com>
2017-09-15 09:47:51 +0800

    producer

---
+++ src/main/java/cn/lzq/mq/transaction/AfterCommitExecutor.java
@@ -0,0 +1,9 @@
+package cn.lzq.mq.transaction;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Created by lianzhiqiang on 17/9/14.
+ */
+public interface AfterCommitExecutor extends Executor {
+}
